d9f5fc2e614017c09f0383c71f8271760097046f
Alonso Betancourt <nicolas.alonso@allianz.com>
2026-06-30 09:53:28 +0200

    minor wins: nvim signcolumn+sensible opts, vim.filetype.add, vim.uv+clone-check, timeoutlen; zsh drop INC_APPEND+$HOME appdir; omp remove git status counts (branch only)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,4 @@
-vim.cmd [[
-  autocmd BufRead,BufNewFile Jenkinsfile* set filetype=groovy
-]]
+vim.filetype.add({ pattern = { ["Jenkinsfile.*"] = "groovy" } })
 
 -- Leader key
 vim.g.mapleader = ' '
@@ -8,8 +6,16 @@ vim.g.maplocalleader = ' '
 
 -- Bootstrap Lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim", lazypath})
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable",
+        "https://github.com/folke/lazy.nvim", lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+        }, true, {})
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -29,3 +35,15 @@ vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+
+-- Sensible defaults
+vim.opt.signcolumn = "yes"        -- always show gutter so it doesn't jump on diagnostics
+vim.opt.ignorecase = true
+vim.opt.smartcase = true          -- case-sensitive only when search has uppercase
+vim.opt.scrolloff = 8
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.updatetime = 250          -- faster CursorHold / diagnostics
+vim.opt.undofile = true           -- persistent undo across sessions
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.timeoutlen = 400          -- snappier which-key popup
