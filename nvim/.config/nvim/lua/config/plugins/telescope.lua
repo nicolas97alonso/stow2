0d7c0f9ff06142a34390adcd93066b5a0fe9a4a3
Alonso Betancourt <nicolas.alonso@allianz.com>
2026-07-09 15:42:27 +0200

    Show filename only in telescope results
    tail avoids the right-aligned horizontal shifting of smart/truncate.

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
         defaults = {
           prompt_prefix = "  ",
           selection_caret = " ",
-          path_display = { "smart" },
+          path_display = { "tail" },
           sorting_strategy = "ascending",
           layout_config = {
             prompt_position = "top",
